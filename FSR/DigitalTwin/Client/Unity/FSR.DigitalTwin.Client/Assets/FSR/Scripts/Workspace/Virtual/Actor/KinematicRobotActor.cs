a7fa388aec9cfe8fbfcc1d5f26148d9e37503a42
Raoul Zebisch <neroware@freenet.de>
2024-10-18 14:58:05 +0200

    Kinematic Robot Actor

--- FSR/DigitalTwin/Client/Unity/FSR.DigitalTwin.Client/Assets/FSR/Scripts/Workspace/Virtual/Actor/KinematicRobotActor.cs
+++ FSR/DigitalTwin/Client/Unity/FSR.DigitalTwin.Client/Assets/FSR/Scripts/Workspace/Virtual/Actor/KinematicRobotActor.cs
@@ -1,6 +1,6 @@
 namespace FSR.DigitalTwin.Client.Unity.Workspace.Virtual.Actor {
 
-    public class CobotKinematikActor : DigitalTwinActorBase {
+    public class KinematicRobotActor : DigitalTwinActorBase {
         
     }
 
