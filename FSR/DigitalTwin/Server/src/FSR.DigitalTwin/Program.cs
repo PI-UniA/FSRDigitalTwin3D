304b2bc9604d8c479efd9c6bae4dcbed31d35c18
Raoul Zebisch <neroware@freenet.de>
2025-05-09 13:46:15 +0200

    Load SOHO ontology

--- FSR/DigitalTwin/Server/src/FSR.DigitalTwin/Program.cs
+++ FSR/DigitalTwin/Server/src/FSR.DigitalTwin/Program.cs
@@ -1,10 +1,30 @@
+// Comment out definitions if unwanted
+#define ENABLE_ONTOLOGY
+
 using AasSecurity;
+using FSR.DigitalTwin.App.Interfaces.Services.Semantic;
 
 Console.WriteLine("AASX Server Core starting....");
 var host = CreateHostBuilder(args).Build();
 
 AasxServer.Program.Main(args);
 SecurityHelper.SecurityInit();
+
+#if ENABLE_ONTOLOGY // Enable OWL ontology
+const int OWL_ONTOLOGY_TRIPLE_COUNT = 644;
+var serviceProvider = host.Services;
+var ontology = serviceProvider.GetService<IOwlOntologyService>()
+    ?? throw new NullReferenceException("should not happen");
+// await ontology.DeleteOntologyAsync();
+if (ontology.Count == OWL_ONTOLOGY_TRIPLE_COUNT) {
+    await ontology.CreateOntologyAsync();
+    // We also want to load the SOHO ontology for human-robot-collaboration if present
+    string sohoOntologyPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "../../modules/SOHO/core/soho_core.owl");
+    if (File.Exists(sohoOntologyPath)) {
+        await ontology.CreateOntologyFromFileAsync(sohoOntologyPath, "application/rdf+xml");
+    }
+}
+#endif
 
 // Run file
 await host.RunAsync();
