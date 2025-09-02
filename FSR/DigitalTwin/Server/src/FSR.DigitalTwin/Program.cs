df2d4dacf82a8f823b8229fa95ae07a907696f2b
Raoul Zebisch <raoul.zebisch@uni-a.de>
2025-09-02 15:41:47 +0200

    Using XML/RDF format consistently to load ontology model

--- FSR/DigitalTwin/Server/src/FSR.DigitalTwin/Program.cs
+++ FSR/DigitalTwin/Server/src/FSR.DigitalTwin/Program.cs
@@ -19,7 +19,6 @@ SecurityHelper.SecurityInit();
 #if ENABLE_ONTOLOGY_MODEL // Enable OWL ontology model
 var ontoModel = host.Services.GetService<IOntologyModelService>()
     ?? throw new NullReferenceException("should not happen");
-var tripleCount = ontoModel.Count;
 var ontoOptions = host.Services.GetRequiredService<IOptions<JenaSemanticDataRepositoryOptions>>().Value;
 await ontoModel.DeleteOntologyModelAsync();
 foreach (string modelFile in ontoOptions.ModelFiles)
@@ -30,7 +29,7 @@ foreach (string modelFile in ontoOptions.ModelFiles)
     await ontoModel.LoadOntologyModelAsync(ontoModelPath, OntologyModelFileFormat.RDF_XML);
 }
 await RunOntologyDemoQueriesAsync(ontoModel);
-Console.WriteLine("Number of triples loaded in semantic database: " + tripleCount);
+Console.WriteLine($"Number of triples loaded in semantic database: {ontoModel.Count}");
 #endif
 
 // Run file
